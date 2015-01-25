5d20f9595f1884d42cb9610d17d5f5b702873c09
gravityshouldbenaut <gravityshouldbenaut@Samarths-MacBook-Pro.local>
2015-01-24 21:44:07 -0800

    madethisone
    made this one

---
+++ app/src/main/java/com/example/motoar/LiveCardMenuActivity.java
@@ -0,0 +1,84 @@
+package com.example.motoar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * A transparent {@link Activity} displaying a "Stop" options menu to remove the {@link com.google.android.glass.timeline.LiveCard}.
+ */
+
+
+public class LiveCardMenuActivity extends Activity implements SensorEventListener {
+    private ImageView image;
+    private TextView speed;
+    private float currentDegree = 0f;
+    private SensorManager mSensorManager;
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        // Open the options menu right away.
+        openOptionsMenu();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        image = (ImageView) findViewById(R.id.compassView);
+        speed = (TextView) findViewById(R.id.speed);
+        speed.setText(new GPS_ListenerService().getSpeed());
+        getMenuInflater().inflate(R.menu.live_card, menu);
+        mSensorManager.registerListener((SensorEventListener) this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+                SensorManager.SENSOR_DELAY_GAME);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_stop:
+                // Stop the service which will unpublish the live card.
+                stopService(new Intent(this, LiveCardService.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onOptionsMenuClosed(Menu menu) {
+        super.onOptionsMenuClosed(menu);
+        // Nothing else to do, finish the Activity.
+        finish();
+    }
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float degree = Math.round(sensorEvent.values[0]);
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree,
+                -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF,
+                0.5f);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+
+        image.startAnimation(ra);
+        currentDegree = -degree;
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+}
